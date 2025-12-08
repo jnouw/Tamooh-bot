f13726d7e178c12a1a1568f1e1348f99a0b65cc7
Qimah-HQ <github@qimah.net>
2025-12-08 05:13:11 +0000

    changed empty room timeout duration

--- services/study/config.js
+++ services/study/config.js
@@ -7,7 +7,7 @@ export const STUDY_ROLE_ID = "1443203557628186755";
 export const TAMOOH_ROLE_ID = "1367043626806542336";
 export const OWNER_ID = "274462470674972682";
 
-export const EMPTY_TIMEOUT_MS = 60 * 1000; // 1 minute
-export const DELETE_DELAY_MS = 60 * 1000; // 60 seconds after completion
+export const EMPTY_TIMEOUT_MS = 30 * 1000; // 20 seconds after completion
+export const DELETE_DELAY_MS = 20 * 1000; // 20 seconds after completion
 export const GROUP_QUEUE_THRESHOLD = 3; // Number of users needed to start group session
 export const QUEUE_TIMEOUT_MS = 10 * 60 * 1000; // 10 minutes - auto-start queue if not full
